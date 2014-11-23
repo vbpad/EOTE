2ab02918f226f62f9a5c879508f4f7f8224bac46
vbpad <vbpad@nbnet.nb.ca>
2014-11-23 08:41:41 -0400

    Update Obligation.js

--- Obligation.js
+++ Obligation.js
@@ -1,21 +1,25 @@
 /* --------------------------------------------------------------------------------------------------- */
-//                                          Obligation Check Script                                          
+//                                          Obligation, Duty, Morality Check Script                                          
 //                                                                                                     
-// This script will Roll a die to check for obligation within the Star Wars EOTE RPG system.
-//        !obligation-check will check all the selected tokens for character sheets then pull the 
-//        required information to check for obligation (even if the character has multiple types).    
-//        it will then roll a die and sort the characters obligation by value, assign the characters     
-//        a bracket and then compare the die roll result to determine if obligation is triggered.    
+// This script will Roll a die to check for obligation, Duty or Morality within the Star Wars EOTE RPG system.
+//        !obligation-check
+//        !duty-check
+//        !morality-check
+//        will check all the selected tokens for character sheets then pull the 
+//        required information to check for the required check (even if the character has multiple types).    
+//        it will then roll a die and sort the characters check by value, assign the characters     
+//        a bracket and then compare the die roll result to determine if type is triggered.    
 //        Finally output the results on the chat window.                                                                                    
 /* --------------------------------------------------------------------------------------------------- */
  
-var ObligDie = 100;                  // Type of die to roll for Oblig
-var ObligAttribute1 = "obligationmag1";  // Attribute to compare the roll
-var ObligAttribute2 = "obligationmag2";  // Attribute to compare the roll
-var ObligAttribute3 = "obligationmag3";  // Attribute to compare the roll
-var ObligAttribute4 = "obligationmag4";  // Attribute to compare the roll
+var Die = 100; // Type of die to roll for Oblig
+var Mor_die = 10;
+var ObligAttribute = "obligationmag";  // Attribute to compare the roll
+var DutyAttribute = "attr_dutymag";
+var Moralityattribute = "attr_morality";
 state.aChar = state.aChars || {};
 var aChars;
+var type;
  
 on("ready", function() 
 {
@@ -28,12 +32,15 @@ on("ready", function()
 on('chat:message', function(msg) 
 {
     var command = msg.content.toLowerCase();
+    /*-------------------------------------------------------------------*/
+    //Obligation Check
     if (msg.type == "api" && command == "!obligation-check")
     {
+        type = "Obligation";
         var selected = msg.selected;
         aChars.length = 0;
 //collect selected tokens and assign obligation values from character sheets
-        createArray(selected, false);
+        characterArray(selected, ObligAttribute);
 // check if any tokens are selected
         if(aChars.length>0)
         {
@@ -46,54 +53,128 @@ on('chat:message', function(msg)
             sendChat("Script", "/w GM No tokens selected!");
         }    
     }
+    /*-------------------------------------------------------------------*/
+    //Duty Check
+    if (msg.type == "api" && command == "!duty-check")
+    {
+        type = "Duty";
+        var selected = msg.selected;
+        aChars.length = 0;
+//collect selected tokens and assign duty values from character sheets
+        characterArray(selected, DutyAttribute);
+// check if any tokens are selected
+        if(aChars.length>0)
+        {
+            sendChat("GM", "/desc Duty");
+//roll the dice and compare results
+            rollandsort();
+        }
+        else
+        {
+            sendChat("Script", "/w GM No tokens selected!");
+        }    
+    }
+    /*-------------------------------------------------------------------*/
+    //Morality Check
+    if (msg.type == "api" && command == "!morality-check")
+    {
+        type = "Morality";
+        var selected = msg.selected;
+        aChars.length = 0;
+//collect selected tokens and assign duty values from character sheets
+        characterArray(selected, Moralityattribute);
+// check if any tokens are selected
+        if(aChars.length>0)
+        {
+            sendChat("GM", "/desc Morality");
+//roll the dice and compare results
+            Morrollandcheck();
+        }
+        else
+        {
+            sendChat("Script", "/w GM No tokens selected!");
+        }    
+    }
 });
  
-function rollandsort(addtokens)
+function rollandsort()
 {
     var dieRolled;
     var Triggered = 0;
 //Rolling dice
-    dieRolled = randomInteger(ObligDie);
-    var chatMsg = "/direct Obligation rolled:" + dieRolled + "<br>";
-    var obligbrackets = new Array()
+    dieRolled = randomInteger(Die);
+    var chatMsg = "/direct " + type + " rolled:" + dieRolled + "<br>";
+    var brackets = new Array()
 // sorts characters by Obligation  
-  aChars = aChars.sort(function(a,b)
- { 
-              if (a[2] < b[2]) return  1;
-                if (a[2] > b[2]) return -1;
-                return 0;
-            });
+    aChars = aChars.sort(function(a,b)
+    { 
+        if (a[2] < b[2]) return  1;
+        if (a[2] > b[2]) return -1;
+        return 0;
+    });
 //Assign character obligations into brackets
     for (var a = 0; a < aChars.length; a++)
     {
-       obligbrackets[a] = new Array()
+       brackets[a] = new Array()
 //pull first characters obligation and display
        if(a>0)
        {
-            obligbrackets[a][1] = obligbrackets[(a-1)][2] + 1
-            obligbrackets[a][2] = obligbrackets[a][1] + aChars[a][2] -1
+            brackets[a][1] = brackets[(a-1)][2] + 1
+            brackets[a][2] = brackets[a][1] + aChars[a][2] -1
        }
 //pull the rest of the characters obligations and display
        else
        {
-            obligbrackets[a][1] = 0
-            obligbrackets[a][2] = aChars[a][2]
+            brackets[a][1] = 0
+            brackets[a][2] = aChars[a][2]
        }
 //if it hasn't triggered yet check if it will
        if (!Triggered)
        {
-            if ((dieRolled >= obligbrackets[a][1]) && (dieRolled <= obligbrackets[a][2]))
+            if ((dieRolled >= brackets[a][1]) && (dieRolled <= brackets[a][2]))
             {
-                var Trigger = aChars[a][3] + "'s Obligation is triggered";
+                var Trigger = aChars[a][3] + "'s " + type + " is triggered";
                 Triggered = 1;
             }
             else
             {
-                Trigger = "No obligation triggered";
+                Trigger = "No " + type + " triggered";
             }
        }
 //display the dice roll list of obligations and the trigger if any
-       chatMsg += aChars[a][3] + " = " + obligbrackets[a][1] + "-" + obligbrackets[a][2] + "<br>";
+       chatMsg += aChars[a][3] + " = " + brackets[a][1] + "-" + brackets[a][2] + "<br>";
+    }
+    chatMsg += "<br>" + Trigger;
+    sendChat("GM", chatMsg);
+  state.aChars = aChars;
+}
+ 
+function Morrollandcheck()
+{
+    var dieRolled;
+    var Trigger = "";
+//Rolling dice
+    dieRolled = (_.random(1 , Mor_die) - 1);
+    var chatMsg = "/direct " + type + " rolled:" + dieRolled + "<br>";
+//display character Moralities
+    for (var a = 0; a < aChars.length; a++)
+    {
+//check if the character's morality triggers
+        var rolltest = aChars[a][2] - (~~(aChars[a][2] / 10) * 10); 
+        if (dieRolled == rolltest)
+        {
+            Trigger += aChars[a][3] + "'s " + type + " is triggered" + "<br>";
+            var Check = 1;
+        }
+        else 
+        {
+            if (!Check)Trigger = "No " + type + " triggered"; 
+        }
+//display the dice roll, list of Moralities and trigger if any
+        if (aChars[a][3])
+        {
+            chatMsg += aChars[a][3] + " = "  + aChars[a][2] + "<br>";
+        }
     }
     chatMsg += "<br>" + Trigger;
     sendChat("GM", chatMsg);
@@ -102,15 +183,9 @@ function rollandsort(addtokens)
  
 
  
-
- 
-function createArray(ob, newtokens)
+function characterArray(ob, attribute)
 {
   var nCount = 0;
-  if(newtokens)
-  {
-    nCount = aChars.length;
-  }
 //pull the obligation values from all the characters selected
   _.each(ob, function(obj)
   {
@@ -121,32 +196,35 @@ function createArray(ob, newtokens)
         if (oCharacter != "")
         { 
             aChars[nCount] = new Array(4);
+            aChars[nCount][2] = 0;
 //assign all the characters information for future use
-            aChars[nCount][0] = token.get('_id');
-            aChars[nCount][3] = token.get('name');
-            var oOblig = findObjs({name: ObligAttribute1, _type: "attribute", _characterid: oCharacter.id}, {caseInsensitive: true})[0];
-            var oOblig2 = findObjs({name: ObligAttribute2, _type: "attribute", _characterid: oCharacter.id}, {caseInsensitive: true})[0];
-            var oOblig3 = findObjs({name: ObligAttribute3, _type: "attribute", _characterid: oCharacter.id}, {caseInsensitive: true})[0];
-            var oOblig4 = findObjs({name: ObligAttribute4, _type: "attribute", _characterid: oCharacter.id}, {caseInsensitive: true})[0];
+            for (var c = 0; c < 5; c++)
+            {
+                if (attribute != Moralityattribute)
+                {
+                    var Attributenum = attribute + "" + c;
+                }
+                else
+                {
+                    var Attributenum = attribute;
+                    aChars[nCount][2] = 0;
+                }
+                var oAttrib = findObjs({name: Attributenum, _type: "attribute", _characterid: oCharacter.id}, {caseInsensitive: true})[0];
 //check if they even have any obligation and how much
-            if (oOblig)
-            {
-                aChars[nCount][1] = "";
-                aChars[nCount][2] = parseInt(oOblig.get('current'));
+                if (oAttrib)
+                {
+                    aChars[nCount][1] = "";
+                    aChars[nCount][2] += parseInt(oAttrib.get('current'));
+                    aChars[nCount][0] = token.get('_id');
+                    aChars[nCount][3] = token.get('name');
+                    var addition = true; 
+                }
             }
-            if (oOblig2)
-            {
-                aChars[nCount][2] += parseInt(oOblig2.get('current'));
-            }
-             if (oOblig3)
-            {
-                aChars[nCount][2] += parseInt(oOblig3.get('current'));
-            }
-             if (oOblig4)
-            {
-                aChars[nCount][2] += parseInt(oOblig4.get('current'));
-            }
-            nCount++;
+             if (addition)
+             {
+                nCount++;
+                addition = false;
+             }
         }
     });
 }
